e67b2cfb6f5f59ffb8c22da7063921093323803b
Leon Martin <alquimista3301@gmail.com>
2026-08-05 14:07:20 -0600

    Initial Commit

---
+++ dev-alarm/dev_alarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  dev_alarmApp.swift
+//  dev-alarm
+//
+//  Created by Malva on 05/08/26.
+//
+
+import SwiftUI
+
+@main
+struct dev_alarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
